c8cdbb6a31550a52b84719e7fd9d63a717b2ad7d
michael cohen <michael.cohen@netspeed.com.au>
2006-01-07 14:17:16 +1000

    minor build system fixes - added enumeration to network code.

---
+++ src/network/dissect.c
@@ -0,0 +1,228 @@
+/********************************************************
+    This is a python module which dissects a packet dump. Similar to
+    ethereal but much faster.
+********************************************************/
+
+/******************************************************
+# Copyright 2004: Commonwealth of Australia.
+#
+# Developed by the Computer Network Vulnerability Team,
+# Information Security Group.
+# Department of Defence.
+#
+# Michael Cohen <scudette@users.sourceforge.net>
+#
+# ******************************************************
+#  Version: FLAG  $Version: 0.78 Date: Fri Aug 19 00:47:14 EST 2005$
+# ******************************************************
+#
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ******************************************************/
+			       
+#include "packet.h"
+#include "network.h"
+#include "init.h"
+
+#undef _POSIX_C_SOURCE
+
+#include "Python.h"
+
+static PyObject *dissect(PyObject *self, PyObject *args) {
+  PyObject *result;
+  Root root;
+  int link_type;
+  StringIO tmp=CONSTRUCT(StringIO, StringIO, Con, NULL);
+  
+  if(!PyArg_ParseTuple(args, "s#i", &tmp->data, &tmp->size, &link_type)) 
+    return NULL;
+
+  root = CONSTRUCT(Root, Packet, super.Con, NULL);
+  root->link_type = link_type;
+
+  root->super.Read((Packet)root, tmp);
+
+  /** Now pass the opaque type back. When this is gced it will be
+      properly destroyed 
+  */
+  result = PyCObject_FromVoidPtr(root, (void (*)(void *))root->super.destroy);
+
+  return result;
+};
+
+/** Returns the object in field as a python object. field is a string
+    of the format node_name.field_name 
+*/
+static PyObject *get_field(PyObject *self, PyObject *args) {
+  PyObject *result;
+  char *element;
+  char *e;
+  char *property;
+  int len;
+  Packet root;
+  struct struct_property_t *p;
+  
+  if(!PyArg_ParseTuple(args, "Os",  &result,&element)) 
+    return NULL;
+  
+  e=talloc_strdup(NULL, element);
+  root = PyCObject_AsVoidPtr(result);
+  if(!root) 
+    return PyErr_Format(PyExc_RuntimeError, "node is not valid");
+
+  len=strcspn(e, ".");
+  if(len == strlen(e)) {
+    property=e;
+    element=NAMEOF(root);
+  } else {
+    property=e+len+1;
+    element=e;
+  };
+
+  e[len]=0;
+
+  if(Find_Property(&root, &p, element, property)) {
+    void *item;
+    int size=0;
+
+    talloc_free(e);
+
+    /** If there was no property, we just return the node itself as an
+	opaque object 
+    */
+    if(!p) {
+      /** Ensure that we increase root's reference count, because we
+	  will try to free it after it gets gc'd
+      */
+      talloc_increase_ref_count(root);
+
+      return PyCObject_FromVoidPtr(root, (void (*)(void *))root->destroy);
+    };
+
+    item = (void *) ((char *)(root->struct_p) + p->item);
+
+    if(!p->size) {
+      size = *(int *)((char *)(root->struct_p) + p->size_p);
+    } else 
+      size=p->size;
+
+    /** Now code the return value according to the node and property
+	returned 
+    */    
+    switch(p->field_type) {
+    case FIELD_TYPE_PACKET:
+      {
+	Packet node = *(Packet *)item;
+
+	result = PyCObject_FromVoidPtr(node, (void (*)(void *))node->destroy);
+	/** We are about to return another reference, we need to incref
+	    talloc to ensure it does not get destroyed unexpectadly 
+	*/
+	talloc_increase_ref_count(node);
+	break;
+      };
+
+    case FIELD_TYPE_INT:
+    case FIELD_TYPE_INT_X:
+    case FIELD_TYPE_IP_ADDR:
+      result = PyLong_FromUnsignedLong(*(unsigned int *)item); break;
+
+    case FIELD_TYPE_CHAR_X:
+    case FIELD_TYPE_CHAR:
+      result = Py_BuildValue("b", *(unsigned char *)item); break;
+
+    case FIELD_TYPE_SHORT_X:
+    case FIELD_TYPE_SHORT:
+      result = Py_BuildValue("h", *(uint16_t *)item); break;
+
+    case FIELD_TYPE_STRING_X:
+    case FIELD_TYPE_STRING:
+      result = Py_BuildValue("s#",*(unsigned char **)item, size); break;
+
+    case FIELD_TYPE_HEX:
+      result = Py_BuildValue("s#",(unsigned char *)item, size); break;
+
+    default:
+      return PyErr_Format(PyExc_RuntimeError,
+			  "Unable to process field of type %u\n", p->field_type);
+    
+    };
+ 
+    return result;
+  } else {
+
+    talloc_free(e);
+    return PyErr_Format(PyExc_KeyError, 
+			"Can not find field %s.%s", e,property);
+  };
+};
+
+
+/*********************************************************
+    Lists the fields in a Packet object
+**********************************************************/
+static PyObject *list_fields(PyObject *self, PyObject *args) {
+  PyObject *result;
+  Packet root;
+  struct struct_property_t *p;
+  PyObject *pylist;
+  
+  if(!PyArg_ParseTuple(args, "O",  &result)) 
+    return NULL;
+  
+  root = PyCObject_AsVoidPtr(result);
+  if(!root) 
+    return PyErr_Format(PyExc_RuntimeError, "node is not valid");
+
+  pylist = PyList_New(0);
+  list_for_each_entry(p, &(root->properties.list), list) {
+    if(p->name) {
+      PyList_Append(pylist, PyString_FromString(p->name));
+    } else break;
+  };
+
+  return pylist;
+};
+
+static PyObject *get_name(PyObject *self, PyObject *args) {
+  PyObject *result;
+  Packet root;
+  
+  if(!PyArg_ParseTuple(args, "O",  &result)) 
+    return NULL;
+  
+  root = PyCObject_AsVoidPtr(result);
+  if(!root) 
+    return PyErr_Format(PyExc_RuntimeError, "node is not valid");
+
+  return PyString_FromString(NAMEOF(root));
+};
+
+
+static PyMethodDef DissectMethods[] = {
+  {"dissect",  dissect, METH_VARARGS,
+   "Dissects a packet returning a dissection object"},
+  {"get_field", get_field, METH_VARARGS,
+   "Gets the field of a dissected node"},
+  {"list_fields", list_fields, METH_VARARGS,
+   "Lists the field names in the dissected object"},
+  {"get_name", get_name, METH_VARARGS,
+   "Returns the name of the current node"},
+  {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC init_dissect(void) {
+  (void) Py_InitModule("_dissect", DissectMethods);
+#include "init.c"
+}
---
+++ src/network/network.c
@@ -0,0 +1,286 @@
+/******************************************************
+# Copyright 2004: Commonwealth of Australia.
+#
+# Developed by the Computer Network Vulnerability Team,
+# Information Security Group.
+# Department of Defence.
+#
+# Michael Cohen <scudette@users.sourceforge.net>
+#
+# ******************************************************
+#  Version: FLAG  $Version: 0.78 Date: Fri Aug 19 00:47:14 EST 2005$
+# ******************************************************
+#
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ******************************************************/
+#include "network.h"
+#include "misc.h"
+#include "pcap.h"
+
+/*** Packers and unpackers for ethernet mac addresses */
+static int Eth2_MAC_pack(char *input, StringIO output) {
+  return CALL(output, write, (char *)(input), 6);
+};
+
+static int Eth2_MAC_unpack(void *context, StringIO input, char *output) {
+  if(CALL(input, read, (char *)(output), 6) < 6)
+    return -1;
+  return 6;
+};
+
+MODULE_INIT(network_structs) {
+  Struct_Register(STRUCT_ETH_ADDR, 6,
+		  Eth2_MAC_pack, Eth2_MAC_unpack);
+};
+
+/****************************************************
+   Root node
+*****************************************************/
+int Root_Read(Packet self, StringIO input) {
+  Root this=(Root)self;
+
+  this->__super__->Read(self, input);
+  
+  switch(this->link_type) {
+  case DLT_EN10MB:
+    this->packet.eth = (Packet)CONSTRUCT(ETH_II, Packet, super.Con, self);
+    return CALL(this->packet.eth, Read, input);
+
+  case DLT_LINUX_SLL:
+    this->packet.eth = (Packet)CONSTRUCT(Cooked, Packet, super.Con, self);
+    return CALL(this->packet.eth, Read, input);
+
+  default:
+    DEBUG("unable to parse link type of %u\n", this->link_type);
+    return -1;
+  };
+};
+
+VIRTUAL(Root, Packet)
+     INIT_STRUCT(packet, q(STRUCT_NULL));
+
+     NAME_ACCESS(packet, eth, FIELD_TYPE_PACKET);
+
+     VMETHOD(super.Read) = Root_Read;
+END_VIRTUAL
+/****************************************************
+   Cooked headers
+*****************************************************/
+int Cooked_Read(Packet self, StringIO input) {
+  Cooked this=(Cooked)self;
+  int len;
+
+  len=this->__super__->Read(self, input);
+
+  switch(this->packet.type) {
+  case 0x800:
+    this->packet.payload = (Packet)CONSTRUCT(IP, Packet, super.Con, self);
+    len += CALL(this->packet.payload, Read, input);
+    break;
+
+  default:
+#ifdef __VERBOSE_DEBUG__
+    DEBUG("Unknown ethernet payload type 0x%x.\n", 
+	  this->packet.type);
+#endif
+    break;
+  };
+
+  return len;
+};
+
+VIRTUAL(Cooked,Packet)
+     INIT_STRUCT(packet, cooked_Format);
+
+     NAME_ACCESS(packet, type, FIELD_TYPE_SHORT_X);
+     NAME_ACCESS(packet, payload, FIELD_TYPE_PACKET);
+
+     VMETHOD(super.Read) = Cooked_Read;
+END_VIRTUAL
+
+/****************************************************
+   Ethernet headers
+*****************************************************/
+int Eth2_Read(Packet self, StringIO input) {
+  ETH_II this=(ETH_II)self;
+  int len;
+
+  /** Call our superclass's Read method - this will populate most of
+      our own struct. 
+      
+      We will automatically consume as much of input as we can handle
+      so far.
+  */
+  len=this->__super__->Read(self, input);
+
+  /** Now depending on the ethernet type we dispatch another parser */
+  switch(this->packet.type) {
+  case 0x800:
+    this->packet.payload = (Packet)CONSTRUCT(IP, Packet, super.Con, self);
+    len += CALL(this->packet.payload, Read, input);
+    break;
+
+  default:
+#ifdef __VERBOSE_DEBUG__
+    DEBUG("Unknown ethernet payload type 0x%x.\n", 
+	  this->packet.type);
+#endif
+    break;
+  };
+
+  return len;
+};
+
+VIRTUAL(ETH_II, Packet)
+     INIT_STRUCT(packet, ethernet_2_Format);
+
+     NAME_ACCESS(packet, destination, FIELD_TYPE_HEX);
+     NAME_ACCESS(packet, source, FIELD_TYPE_HEX);
+     NAME_ACCESS(packet, type, FIELD_TYPE_SHORT_X);
+     NAME_ACCESS(packet, payload, FIELD_TYPE_PACKET);
+
+     NAMEOF(this) = "eth";
+     VMETHOD(super.Read) = Eth2_Read;
+END_VIRTUAL
+
+/****************************************************
+   IP header
+*****************************************************/
+int IP_Read(Packet self, StringIO input) {
+  IP this=(IP)self;
+  int len;
+
+  len=this->__super__->Read(self, input);
+
+  /** Now choose the dissector for the next layer */
+  switch(this->packet.protocol) {
+  case 0x6:
+    this->packet.payload = (Packet)CONSTRUCT(TCP, Packet, super.Con, self);
+    break;
+
+  case 0x11:
+    this->packet.payload = (Packet)CONSTRUCT(UDP, Packet, super.Con, self);
+    break;
+    
+  default:
+#ifdef __VERBOSE_DEBUG__
+    DEBUG("Unknown IP payload type 0x%x.\n", 
+	  this->packet.protocol);
+#endif
+    return len;
+  };
+
+  /** Now we seek to the spot in the input stream where the payload is
+      supposed to start. This could be a few bytes after our current
+      position in case the packet has options that we did not account
+      for.
+  */
+  CALL(input, seek, self->start + this->packet.header_length * 4, 
+       SEEK_SET);
+
+  CALL(this->packet.payload, Read, input);
+
+  return input->readptr - self->start;
+};
+
+VIRTUAL(IP, Packet)
+     INIT_STRUCT(packet, ip_Format);
+
+     NAME_ACCESS(packet, ttl, FIELD_TYPE_CHAR);
+     NAME_ACCESS(packet, protocol, FIELD_TYPE_CHAR);
+     NAME_ACCESS(packet, src, FIELD_TYPE_IP_ADDR);
+     NAME_ACCESS(packet, dest, FIELD_TYPE_IP_ADDR);
+     NAME_ACCESS(packet, payload, FIELD_TYPE_PACKET);
+
+     VMETHOD(super.Read)=IP_Read;
+END_VIRTUAL
+
+/****************************************************
+   TCP header
+*****************************************************/
+int TCP_Read(Packet self, StringIO input) {
+  TCP this=(TCP)self;
+
+  this->__super__->Read(self, input);
+
+  this->packet.len  = this->packet.header_length * 4;
+
+  /** Now we seek to the spot in the input stream where the data
+      payload is supposed to start. This could be a few bytes after
+      our current position in case the packet has options that we did
+      not account for.
+  */
+  this->packet.data_offset = self->start + this->packet.len ;
+  CALL(input, seek, this->packet.data_offset, SEEK_SET);
+
+  /** Now populate the data payload of the tcp packet 
+
+      NOTE: We assume the rest of the packet is all data payload (and
+      there is only 1 packet in the input stream). This is not always
+      true, we really need to go from the IP total length field.
+  */
+  this->packet.data_len = input->size - input->readptr;
+
+  this->packet.data = talloc_memdup(self, input->data + input->readptr,
+				    this->packet.data_len);
+  
+  return input->size - self->start;
+};
+
+VIRTUAL(TCP, Packet)
+     INIT_STRUCT(packet, tcp_Format);
+
+     NAME_ACCESS(packet, src_port, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, dest_port, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, seq, FIELD_TYPE_INT);
+     NAME_ACCESS(packet, ack, FIELD_TYPE_INT);
+     NAME_ACCESS(packet, len, FIELD_TYPE_INT);
+     NAME_ACCESS(packet, flags, FIELD_TYPE_CHAR_X);
+     NAME_ACCESS(packet, window_size, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, data_offset, FIELD_TYPE_INT);
+     NAME_ACCESS(packet, data_len, FIELD_TYPE_INT);
+     NAME_ACCESS_SIZE(packet, data, FIELD_TYPE_STRING, data_len);
+
+     VMETHOD(super.Read) = TCP_Read;
+END_VIRTUAL
+
+/****************************************************
+   UDP Header
+*****************************************************/
+int UDP_Read(Packet self, StringIO input) {
+  UDP this = (UDP) self;
+  int len;
+
+  len =this->__super__->Read(self, input);
+
+  /** UDP has no options, data starts right away. */
+  this->packet.data_len = this->packet.length - len;
+  this->packet.data = talloc_memdup(self, input->data + input->readptr,
+				    this->packet.data_len);
+
+  return this->packet.length;
+};
+
+VIRTUAL(UDP, Packet)
+     INIT_STRUCT(packet, udp_Format);
+
+     NAME_ACCESS(packet, src_port, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, dest_port, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, length, FIELD_TYPE_SHORT);
+     NAME_ACCESS(packet, checksum, FIELD_TYPE_SHORT_X);
+     NAME_ACCESS_SIZE(packet, data, FIELD_TYPE_STRING, data_len);
+
+     VMETHOD(super.Read) = UDP_Read;
+END_VIRTUAL
