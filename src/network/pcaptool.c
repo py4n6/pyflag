34cc1714a80bda09575c267d6e3899035b340768
michael cohen <michael.cohen@netspeed.com.au>
2006-01-12 22:24:14 +1000

    Network dissector can now handle garbage at the end of packets

---
+++ src/network/pcaptool.c
@@ -0,0 +1,170 @@
+/****************************************************************
+   Uses libpcap to parse pcap files and emit SQL to index them.
+
+   We queue up many packets so that we can emit a single SQL insert
+   for all of them. Using sql extended insert syntax makes the db
+   _much_ faster.
+*****************************************************************/
+/******************************************************
+# Copyright 2004: Commonwealth of Australia.
+#
+# Developed by the Computer Network Vulnerability Team,
+# Information Security Group.
+# Department of Defence.
+#
+# Michael Cohen <scudette@users.sourceforge.net>
+#
+# ******************************************************
+#  Version: FLAG  $Version: 0.78 Date: Fri Aug 19 00:47:14 EST 2005$
+# ******************************************************
+#
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ******************************************************/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include <unistd.h>
+
+#define TRUE 1
+
+struct packet_data_t {
+  long unsigned int packet_id;
+  long unsigned int data_offset;
+  int caplen;
+  long unsigned int sec;
+  long unsigned int usec;
+  int encap;
+};
+
+#define MAX_BUFF_SIZE 1024
+
+static struct packet_data_t buffer[MAX_BUFF_SIZE+1];
+static int buffer_count=0;
+
+//We start counting packets from one so we are matched to ethereal.
+long unsigned int packet_id = 1;
+pcap_t *pfh;
+char *tableName = NULL;
+
+void print_buffer(char *tableName) {
+  int i;
+  struct packet_data_t *p=buffer;
+
+  printf("INSERT INTO `%s` VALUES (%lu,%lu,%u,%lu,%lu,%u)",tableName,
+	 p->packet_id, p->data_offset, p->caplen,p->sec,p->usec,p->encap);
+
+  for(i=1;i<buffer_count;i++) {
+    p=&buffer[i];
+
+    printf(",(%lu,%lu,%u,%lu,%lu,%u)", 
+	   p->packet_id, p->data_offset, p->caplen,p->sec,p->usec,p->encap);
+  };
+  printf(";\n");
+};
+
+void printUsage() {
+  printf ("\nUSAGE: $ ./pcaptool -t tablename [-c] inputfile [inputfile]...\n");
+  printf ("-c: create new tables before insertion\n\n");
+}
+
+
+void handler(u_char *data, const struct pcap_pkthdr *header, 
+	     const u_char *user_data) {
+  FILE *fp = pcap_file(pfh);
+  int data_offset = ftell(fp);
+  int pkt_encap = pcap_datalink(pfh);
+  
+  buffer[buffer_count].packet_id = packet_id;
+  buffer[buffer_count].data_offset=data_offset - (int) header->caplen;
+  buffer[buffer_count].caplen = (int) header->caplen;
+  buffer[buffer_count].sec = (long unsigned int)header->ts.tv_sec;
+  buffer[buffer_count].usec= (long unsigned int)header->ts.tv_usec;
+  buffer[buffer_count].encap= pkt_encap;
+  buffer_count++;
+  
+  if(buffer_count>=MAX_BUFF_SIZE) {
+    print_buffer(tableName);
+    buffer_count=0;
+  };
+  packet_id++;
+};
+
+int main(int argc, char **argv) {
+  char err_info[PCAP_ERRBUF_SIZE];
+  char *fname=NULL;
+  int file_id = 0;
+  int createNewTable = 0;
+  int index, c;
+
+  while ((c = getopt (argc, argv, "t:c")) != -1) {
+    switch(c) {
+    case 'c':
+      createNewTable = TRUE;
+      break;
+    case 't':
+      tableName = optarg;
+      break;
+    default:
+      printUsage();
+      exit(0);
+    };
+  };
+
+  if ((argc < 2) || (tableName == NULL)) {
+    printUsage();
+    exit(0);
+  }
+
+  if (createNewTable == TRUE) {
+    /* will prob need to do some validation of tableName */
+    printf ("CREATE TABLE `%s` ( \n", tableName);
+    printf ("  `id` INT NOT NULL , \n");
+    printf ("  `offset` INT NOT NULL , \n");
+    printf ("  `length` INT NOT NULL , \n");
+    printf ("  `ts_sec` INT NOT NULL , \n");
+    printf ("  `ts_usec` INT NOT NULL, \n");
+    printf ("  `link_type`  TINYINT not null\n");
+    printf ("); \n\n");
+
+    exit(0);
+  }
+  
+  //sleep(10);
+  
+  /* If there's an error with an input file, print an error and then
+     try the next file */
+  for (index = optind; index < argc; index++) {
+    fname = argv[index];
+    pfh = pcap_open_offline(fname, err_info);
+    
+    if (pfh == NULL) {
+      fprintf(stderr,"Unable to open %s as a pcap file: %s. Quitting\n", 
+	      fname, err_info);
+      exit(-1);
+    } else {
+      pcap_dispatch(pfh, -1, handler, NULL);
+
+      //Get the left over packets in the buffer
+      if(buffer_count>0)
+	print_buffer(tableName);
+      
+      file_id++;
+      pcap_close(pfh);
+    }
+  }
+
+  return (0);
+}
---
+++ src/plugins/DiskForensics/FileHandlers/ZipFile.py
@@ -0,0 +1,259 @@
+# Michael Cohen <scudette@users.sourceforge.net>
+# David Collett <daveco@users.sourceforge.net>
+#
+# ******************************************************
+#  Version: FLAG $Version: 0.78 Date: Fri Aug 19 00:47:14 EST 2005$
+# ******************************************************
+#
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# ******************************************************
+""" This module provides support for compressed file formats such as Zip and Gzip.
+
+The scanner recurses into zip files, executing the scanner factory train on files within the ZIP file.
+
+This feature complements the ZIP and Gzip filesystem driver to ensure that zip and gzip files are transparently viewable by the FLAG GUI.
+"""
+import os.path
+import pyflag.logging as logging
+from pyflag.Scanner import *
+import zipfile,gzip,tarfile
+from pyflag.FileSystem import File , CachedFile
+import pyflag.FlagFramework as FlagFramework
+import time,re,os
+import StringIO
+import pyflag.Scanner as Scanner
+import gzip
+
+class ZipScan(GenScanFactory):
+    """ Recurse into Zip Files """
+    order=99
+    default = True
+    depends = 'TypeScan'
+    
+    def destroy(self):
+        pass
+    
+    class Scan(StoreAndScanType):
+        types = (
+            'application/x-zip',
+            )
+
+        def external_process(self,name):
+            """ This is run on the extracted file """
+            zip=zipfile.ZipFile(name)
+            
+            ## List all the files in the zip file:
+            dircount = 0
+            namelist = zip.namelist()
+            for i in range(len(namelist)):
+                ## Add the file into the VFS
+                try:
+                    ## Convert the time to a common format.
+                    t = time.mktime(list(zip.infolist()[i].date_time) +[0,0,0])
+                except:
+                    t=0
+
+                ## If the entry corresponds to just a directory we ignore it.
+                if not os.path.basename(namelist[i]): continue
+
+                self.ddfs.VFSCreate(self.inode,"Z%s" % i,namelist[i],size=zip.infolist()[i].file_size,mtime=t)
+
+                ## Now call the scanners on this new file (FIXME limit the recursion level here)
+                fd = StringIO.StringIO(zip.read(namelist[i]))
+                fd.inode = "%s|Z%s" % (self.inode,i)
+                Scanner.scanfile(self.ddfs,fd,self.factories)
+
+class GZScan(ZipScan):
+    """ Decompress Gzip files """
+
+    class Drawer(Scanner.Drawer):
+        description = "Compressed file support"
+        name = "Compressed File"
+        contains = ['GZScan','TarScan','ZipScan']
+        default = False
+        
+    class Scan(ScanIfType):
+        """ If we hit a gzip file, we just create a new Inode entry in the VFS """
+        types = (
+            'application/x-gzip' ,
+            )
+        
+        def __init__(self, inode,ddfs,outer,factories=None,fd=None):
+            ScanIfType.__init__(self, inode,ddfs,outer,factories)
+            self.filename = None
+
+        def process(self, data, metadata=None):
+            ScanIfType.process(self,data,metadata)
+            if not self.boring_status and not self.filename:
+                ## We need to find the name of the original uncompressed
+                ## file so we can set a sensible VFS file name. This is
+                ## the algorithm used:
+                ## 1) We try to decompress the first data block from the file to see if the original name is in the header
+                ## 2) Failing this we check if the inodes filename ends with .gz
+                ## 3) Failing that, we call the new file "data"
+                a=FlagFramework.Magic()
+                a.buffer(data)
+                match = re.search(a.magic,'was "([^"]+)"')
+                if match:
+                    self.filename = match.groups(1)
+                    return
+
+                original_filename = os.path.basename(self.ddfs.lookup(inode=self.inode))
+                if original_filename.endswith(".gz"):
+                    self.filename=original_filename[:-3]
+                    return
+
+                self.filename="Uncompressed"
+
+        def finish(self):
+            if self.filename:
+                self.ddfs.VFSCreate(self.inode,"G0",self.filename)
+
+                new_inode="%s|G0" % (self.inode)
+                ## Scan the new file using the scanner train:
+                fd=self.ddfs.open(inode=new_inode)
+                Scanner.scanfile(self.ddfs,fd,self.factories)
+                fd.close()            
+
+class TarScan(GenScanFactory):
+    """ Recurse into Tar Files """
+    order=99
+    default = True
+    depends = [ 'TypeScan' ]
+
+    def destroy(self):
+        pass
+    
+    class Scan(StoreAndScanType):
+        types = (
+            'application/x-tar',
+            )
+
+        def external_process(self,name):
+            """ This is run on the extracted file """
+	        #Get a TarFile object
+            tar=tarfile.TarFile(name)
+            
+            ## List all the files in the tar file:
+            dircount = 0
+            namelist = tar.getnames()
+            for i in range(len(namelist)):
+
+                ## If the entry corresponds to just a directory we ignore it.
+                if not os.path.basename(namelist[i]): continue
+                
+                ## Add the file into the VFS
+                self.ddfs.VFSCreate(
+                    self.inode,"T%s" % i,namelist[i],
+                    size=tar.getmember(namelist[i]).size,
+                    mtime=tar.getmember(namelist[i]).mtime,
+                    uid=tar.getmember(namelist[i]).uid,
+                    gid=tar.getmember(namelist[i]).gid,
+                    mode=oct(tar.getmember(namelist[i]).mode),
+                    )
+                
+                new_inode="%s|T%s" % (self.inode,i)
+                ## Scan the new file using the scanner train:
+                fd=self.ddfs.open(inode=new_inode)
+                Scanner.scanfile(self.ddfs,fd,self.factories)
+                fd.close()
+		
+## These are the corresponding VFS modules:
+class Zip_file(File):
+    """ A file like object to read files from within zip files. Note that the zip file is specified as an inode in the DBFS """
+    specifier = 'Z'
+    
+    def __init__(self, case, table, fd, inode):
+        File.__init__(self, case, table, fd, inode)
+        # strategy:
+        # inode is the index into the namelist of the zip file (i hope this is consistant!!)
+        # just read that file!
+        parts = inode.split('|')
+        try:
+            z = zipfile.ZipFile(fd,'r')
+            self.data = z.read(z.namelist()[int(parts[-1][1:])])
+        except (IndexError, KeyError, zipfile.BadZipfile),e:
+            raise IOError, "Zip_File: (%s)" % e
+        
+        self.readptr=0
+        self.size=len(self.data)
+        
+    def read(self,len=None):
+        if len:
+            temp=self.data[self.readptr:self.readptr+len]
+            self.readptr+=len
+            return temp
+        else: return self.data
+
+    def close(self):
+        pass
+
+class GZ_file(CachedFile):
+    """ A file like object to read gzipped files. """
+    specifier = 'G'
+    def cache(self,fd):
+        self.gz = gzip.GzipFile(fileobj=self.fd)
+        count = 0
+        step = 1024
+        
+        ## Copy ourself into the file - This is in case we have errors
+        ## in the file, we try to read as much as possible:
+        while 1:
+            try:
+                data=self.gz.read(step)
+            except:
+                step /= 2
+                if step<10:
+                    logging.log(logging.DEBUG, "Error reading from %s, could only get %s bytes" % (self.fd.inode, count));
+                    break
+                
+                else:
+                    continue
+            
+            count += len(data)
+            if len(data)==0: break
+            fd.write(data)
+
+        return count
+
+class Tar_file(File):
+    """ A file like object to read files from within tar files. Note that the tar file is specified as an inode in the DBFS """
+    specifier = 'T'
+    
+    def __init__(self, case, table, fd, inode):
+        File.__init__(self, case, table, fd, inode)
+        # strategy:
+        # inode is the index into the namelist of the tar file (i hope this is consistant!!)
+        # just read that file!
+        parts = inode.split('|')
+        try:
+            t = tarfile.TarFile(fileobj=fd)
+            name=t.getnames()[int(parts[-1][1:])]
+            self.data = t.extractfile(name).read()
+        except (IndexError, KeyError):
+            raise IOError, "Tar_File: cant find index"
+        
+        self.readptr=0
+        self.size=t.getmember(name).size
+                
+    def read(self,len=None):
+        if len:
+            temp=self.data[self.readptr:self.readptr+len]
+            self.readptr+=len
+            return temp
+        else: return self.data
+
+    def close(self):
+        pass
