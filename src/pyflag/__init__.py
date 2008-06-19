669e531150fca904fc1b0bd066fdcfba41abf19b
phil h <phil.h@gmx.net>
2008-06-20 07:33:39 +1000

    added interpreter to all python scripts

---
+++ src/pyflag/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+""" Package pyFlag.
+
+PyFlag is the python implementation of FLAG- The Forensic and Log Analysis GUI. A package used to for the rapid and accurate analysis of forensic data.
+"""
